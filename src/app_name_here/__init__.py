34b25a11228373aa9843dab0ec91b9b7300b0230
BraedonHaensel <bhaensel111@gmail.com>
2025-10-23 17:44:24 -0600

    moved database file to src

--- src/app_name_here/__init__.py
+++ src/app_name_here/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Define the local database.
-engine = create_engine('sqlite:///../database/app_name_here.db')
+engine = create_engine('sqlite:///database.db')
 
 # Create the SQLAlchemy session.
 db = sessionmaker(bind=engine)()
